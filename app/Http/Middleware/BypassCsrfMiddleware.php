0e46b5c8f23136d075009c6a6e67be061b16bb2a
Sanjay Udoshi <smudoshi@gmail.com>
2025-03-24 15:39:18 -0400

    Fix method signature in BypassCsrfMiddleware

--- app/Http/Middleware/BypassCsrfMiddleware.php
+++ app/Http/Middleware/BypassCsrfMiddleware.php
@@ -19,9 +19,11 @@ class BypassCsrfMiddleware extends VerifyCsrfToken
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
         // Simply pass the request to the next middleware without CSRF verification
         return $next($request);
