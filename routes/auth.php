51d9d8a7260c9985b4a2661d3253dbe588e5abcd
Sanjay Udoshi <smudoshi@gmail.com>
2025-03-28 01:30:40 -0400

    Exclude login route from CSRF verification

--- routes/auth.php
+++ routes/auth.php
@@ -20,7 +20,8 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])
+        ->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class]);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
