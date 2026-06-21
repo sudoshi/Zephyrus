d5c8841e7413c1a3b188321d362839ec0dccc906
Sanjay Udoshi <smudoshi@gmail.com>
2026-06-21 00:41:56 -0400

    chore(rtdc): add schedule hook for nightly reconciliation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,14 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -22,5 +23,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        // RTDC Step-4 reconciliation registered in Phase F.
     })
     ->create();
